a4ed5e4d04b088b492c7153420f7fea36e3d6c39
stkytomas <stkytomas@gmail.com>
2021-03-09 23:41:01 -0600

    deleted Board's constructor and updated this.state to this.props in renderSquare

--- src/index.js
+++ src/index.js
@@ -24,14 +24,6 @@ import './index.css';
   }
   
   class Board extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            squares: Array(9).fill(null),
-            xIsNext: true,
-        };
-    }
-
     handleClick(i) {
         const squares = this.state.squares.slice();
         if (calculateWinner(squares) || squares[i]) {
@@ -47,8 +39,8 @@ import './index.css';
     renderSquare(i) {
       return (
       <Square 
-      value={this.state.squares[i]}
-      onClick={() => this.handleClick(i)} 
+      value={this.props.squares[i]}
+      onClick={() => this.props.onClick(i)} 
       />
     );
 }
