2c568c48eb37e3ed0e18c55970a88582608f91bf
stkytomas <stkytomas@gmail.com>
2021-03-09 23:45:11 -0600

    updated Game's render to handle the game's status. removed corresponding code from Board's render

--- src/index.js
+++ src/index.js
@@ -45,19 +45,8 @@ import './index.css';
     );
 }
     render() {
-    //   const status = 'Next player: ' + 
-    //   (this.state.xIsNext ? 'X' : 'O');
-    const winner = calculateWinner(this.state.squares);
-    let status;
-    if (winner) {
-      status = 'Winner: ' + winner;
-    } else {
-      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
-    }
-  
       return (
         <div>
-          <div className="status">{status}</div>
           <div className="board-row">
             {this.renderSquare(0)}
             {this.renderSquare(1)}
@@ -90,13 +79,26 @@ import './index.css';
     }
 
     render() {
+        const history = this.state.history;
+        const current = history[history.length - 1];
+        const winner = calculateWinner(current.squares);
+        let status;
+        if (winner) {
+          status = 'Winner: ' + winner;
+        } else {
+          status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+        }
+
       return (
         <div className="game">
           <div className="game-board">
-            <Board />
+            <Board
+            squares={current.squares}
+            onClick={(i) => this.handleClick(i)}
+          />
           </div>
           <div className="game-info">
-            <div>{/* status */}</div>
+            <div>{status}</div>
             <ol>{/* TODO */}</ol>
           </div>
         </div>
